42e8666963fe8613a681035b93b020ef65fff111
Adrian Lam <LamLH@dulwich.org.uk>
2019-06-19 17:03:38 +0100

    Update ProgramArcadeGames Practice2.py

--- ProgramArcadeGames Practice2.py
+++ ProgramArcadeGames Practice2.py
@@ -38,8 +38,7 @@ clock = pygame.time.Clock()
 
 # - - - - - - - - Main program loop - - - - - - - - -
 while not done:
-    # Clear the screen and set white background
-    screen.fill(WHITE)
+
     # - - - Main event loop - - -
     for event in pygame.event.get(): #User did something
         if event.type == pygame.QUIT:
@@ -52,71 +51,71 @@ while not done:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print("User pressed a mouse button")
 
-        # - - - Game logic here - - -
+    # - - - Game logic here - - -
 
-        # - - - Drawing code here - - -
-        # First clear the screen to white. Don't put other drawing commands above this, or
-        # they will be erased with this command
-        screen.fill(WHITE)
+    # - - - Drawing code here - - -
+    # First clear the screen to white. Don't put other drawing commands above this, or
+    # they will be erased with this command
+    screen.fill(WHITE)
 
-        pygame.draw.rect(screen, RED, [55, 50, 20, 25], 1)
-        # Draw green line on screen 5 pixels wide from (0, 0) to (100, 100)
-        pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-        pygame.draw.line(screen, GREEN, [50, 50], [300, 300], 2)
-        # Draw on screen several lines from (0, 10) to (100, 110) 5 pixels wide using while loop
-        y_offset = 0
-        while y_offset < 100:
-            pygame.draw.line(screen, RED, [0, 10 + y_offset], [100, 110 + y_offset], 5)
-            y_offset = y_offset + 10
-        # Complex offsets
-        for index in range(200):
-            radians_x = index/20
-            radians_y = index/6
-            x = int(75 * math.sin(radians_x))
-            y = int(75 * math.cos(radians_y))
+    pygame.draw.rect(screen, RED, [55, 50, 20, 25], 1)
+    # Draw green line on screen 5 pixels wide from (0, 0) to (100, 100)
+    pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    pygame.draw.line(screen, GREEN, [50, 50], [300, 300], 2)
+    # Draw on screen several lines from (0, 10) to (100, 110) 5 pixels wide using while loop
+    y_offset = 0
+    while y_offset < 100:
+        pygame.draw.line(screen, RED, [0, 10 + y_offset], [100, 110 + y_offset], 5)
+        y_offset = y_offset + 10
+    # Complex offsets
+    for index in range(200):
+        radians_x = index/20
+        radians_y = index/6
+        x = int(75 * math.sin(radians_x))
+        y = int(75 * math.cos(radians_y))
 
-        for x_offset in range(30, 300, 30):
-            pygame.draw.line(screen, BLACK, [x_offset, 100], [x_offset - 10, 90], 2)
-            pygame.draw.line(screen, BLACK, [x_offset, 90], [x_offset - 10, 100], 2)
+    for x_offset in range(30, 300, 30):
+        pygame.draw.line(screen, BLACK, [x_offset, 100], [x_offset - 10, 90], 2)
+        pygame.draw.line(screen, BLACK, [x_offset, 90], [x_offset - 10, 100], 2)
 
-            pygame.draw.line(screen, BLACK, [x, y], [x+5, y], 5)
+    pygame.draw.line(screen, BLACK, [x, y], [x+5, y], 5)
 
-        # Draw rectangle, (20, 20) is top left coordinate of rectangle
-        pygame.draw.rect(screen, BLACK, [20, 20, 250, 100], 2)
+    # Draw rectangle, (20, 20) is top left coordinate of rectangle
+    pygame.draw.rect(screen, BLACK, [20, 20, 250, 100], 2)
 
-        # Filled in rectangle
-        pygame.draw.rect(screen, BLACK, [50, 80, 125, 50], 0)
+    # Filled in rectangle
+    pygame.draw.rect(screen, BLACK, [50, 80, 125, 50], 0)
 
-        # Draw an ellipse using rectangle
-        pygame.draw.ellipse(screen, RED, [20, 20, 250, 100], 0)
-        pygame.draw.arc(screen, BLUE, [20, 20, 250, 100], PI / 2, PI, 4)
-        pygame.draw.arc(screen, BLACK, [20, 20, 250, 100], PI, (3/2) * PI, 4)
-        pygame.draw.arc(screen, BLUE, [20, 20, 250, 100], (3/2) * PI, 0, 4)
-        pygame.draw.arc(screen, BLACK, [20, 20, 250, 100], 0, PI / 2, 4)
+    # Draw an ellipse using rectangle
+    pygame.draw.ellipse(screen, RED, [20, 20, 250, 100], 0)
+    pygame.draw.arc(screen, BLUE, [20, 20, 250, 100], PI / 2, PI, 4)
+    pygame.draw.arc(screen, BLACK, [20, 20, 250, 100], PI, (3/2) * PI, 4)
+    pygame.draw.arc(screen, BLUE, [20, 20, 250, 100], (3/2) * PI, 0, 4)
+    pygame.draw.arc(screen, BLACK, [20, 20, 250, 100], 0, PI / 2, 4)
 
-        # Draw a triangle
-        pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
+    # Draw a triangle
+    pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
 
-        # Draw text (Font chosen, size, bold, italics)
-        font = pygame.font.SysFont('Arial', 25, True, False)
-        text = font.render("Test text", True, RED)
-        screen.blit(text, [250, 250])
+    # Draw text (Font chosen, size, bold, italics)
+    font = pygame.font.SysFont('Arial', 25, True, False)
+    text = font.render("Test text", True, RED)
+    screen.blit(text, [250, 250])
 
-        # Draw snow particles
-        for i in range(len(snow_list)):
-            if snow_list[i][0] > 700 or snow_list[i][1] > 500:
-                snow_list[i][0] = random.randrange(0, 500)
-                snow_list[i][1] = 0
-            else:
-                snow_list[i][0] = random.randrange(x_position-20, x_position+40)
-                snow_list[i][1] = random.randrange(y_position-10, y_position+30)
-            pygame.draw.circle(screen, BLUE, snow_list[i], 1)
+    # Draw snow particles
+    for i in range(len(snow_list)):
+        if snow_list[i][0] > 700 or snow_list[i][1] > 500:
+            snow_list[i][0] = random.randrange(0, 500)
+            snow_list[i][1] = 0
+        else:
+            snow_list[i][0] = random.randrange(x_position-20, x_position+60)
+            snow_list[i][1] = random.randrange(y_position-10, y_position+30)
+        pygame.draw.circle(screen, BLUE, snow_list[i], 1)
 
-        # Update screen drawn
-        pygame.display.flip()
+    # Update screen drawn
+    pygame.display.flip()
 
-        # - - - Limit to 60 fps
-        clock.tick(60)
+    # - - - Limit to 60 fps
+    clock.tick(60)
 
 # Shutdown python program
 pygame.quit()
